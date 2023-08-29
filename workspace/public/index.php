a93ddcf22e2faf23f99013c6608f50008b7fd4e2
Lenny606 <thomas.kravcik@gmail.com>
2023-08-29 19:09:03 +0200

    lesson 6

--- workspace/public/index.php
+++ workspace/public/index.php
@@ -1,18 +1,20 @@
 <?php
 
 //loads namespaces , package
+// namespace App\Controller;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use DI\Container;
+use DI\Container; //for dependency injection
 
 //imports all depedencies
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-//create Container
+//create Container for services
 $container = new Container();
 
-//registering new service for Mustache template engine
+//registering new service for Mustache template engine into container
 $container->set('templating', function () {
     return new Mustache_Engine([
         'loader' => new Mustache_Loader_FilesystemLoader(
@@ -21,6 +23,7 @@ $container->set('templating', function () {
         )
     ]);
 });
+
 AppFactory::setContainer($container);
 
 //creates app 
@@ -41,7 +44,7 @@ $app->get('/hello', function (Request $request, Response $response) {
 // route with parameters, third argument needed in function 
 $app->get('/hello/test/{name}', function (Request $request, Response $response, array $args) {
     $name = ucfirst($args['name']);  //ucfirst capitalize first letter 
-    $response->getBody()->write('hello' . " " . $name);
+    $response->getBody()->write('hello, ' . $name);
     return $response;
 });
 
@@ -54,6 +57,17 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     $response->getBody()->write($html);
     return $response;
 });
+
+//wiring the custom Controllers
+//instead of function class name and method name is passed
+//Slim syntax => Controller:method
+$app->get('/homepage', '\App\Controller\FirstController:homepage');
+$app->get('/hello2', '\App\Controller\SecondController:hello');
+$app->get('/albums', '\App\Controller\SearchController:albums');
+$app->get('/search', '\App\Controller\SearchController:search');
+//used method any() => get/post method is choosed 
+$app->any('/form', '\App\Controller\SearchController:form');
+$app->get('/api', '\App\Controller\ApiController:search');
 
 //app is ready and can be runned
 $app->run();
